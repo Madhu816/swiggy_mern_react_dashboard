9221119bd3cc9fe582553c853112f83e0273d830
Madhu816 <madhupiske816@gmail.com>
2025-05-23 19:13:29 +0530

    some add

--- src/venderDashboard/components/Welcome.jsx
+++ src/venderDashboard/components/Welcome.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Welcome = () => {
   return (
     <div>
       <h1>Welcome..</h1>
+      <img src="/uploads/48465204.jpg" alt="chef" />
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
