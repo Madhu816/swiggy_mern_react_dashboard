43bbe7e59ae3ba391ead69cf1279af7bacd0d111
Madhu816 <madhupiske816@gmail.com>
2025-05-24 21:44:58 +0530

    add new

--- src/venderDashboard/components/Welcome.jsx
+++ src/venderDashboard/components/Welcome.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+// import React from 'react'
+
+// const Welcome = () => {
+//     const firmName = localStorage.getItem("firmname")
+
+//   return (
+//     <div className='welcomeSection'>
+//         <h2>Welcome {firmName}</h2>
+//         <div className="landingImage">
+//           <img src='/assets/chefAunty.jpg' alt='welcome' />
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Welcome
+
+
+import React from 'react';
+import chefAunty from '@/assets/chefAunty.jpg'; // alias-based absolute path
 
 const Welcome = () => {
-    const firmName = localStorage.getItem("firmName")
+  const firmName = localStorage.getItem("firmname");
 
   return (
     <div className='welcomeSection'>
-        <h2>Welcome {firmName}</h2>
-        <div className="landingImage">
-          <img src='/assets/chefAunty.jpg' alt='welcome' />
-        </div>
+      <h2>Welcome {firmName || "Guest"}</h2>
+      <div className="landingImage">
+        <img src={chefAunty} alt="welcome" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
+
