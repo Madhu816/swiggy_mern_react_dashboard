13f473f4e0e4ab396889d142eb14119d185a7059
Madhu816 <madhupiske816@gmail.com>
2025-05-25 17:56:19 +0530

    new

--- src/venderDashboard/components/Welcome.jsx
+++ src/venderDashboard/components/Welcome.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Welcome = () => {
-    const firmName = localStorage.getItem("firmname")
+  const rawName = localStorage.getItem("firmname");
+  const firmName = rawName ? rawName.toUpperCase() : "YOUR FIRM";
 
   return (
     <div className='welcomeSection'>
-        <h2>WELCOME TO : {firmName.toUpperCase()}</h2> 
+      <h2>WELCOME TO : {firmName}</h2>
+
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
-
+export default Welcome;
